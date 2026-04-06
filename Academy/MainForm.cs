09e04a3be0aa48665d8fd152fdb8ef8742de7ba5
Николай Немов <prost91@outlook.com>
2026-04-06 22:07:01 +0300

    Students Empty Fields DONE

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -120,7 +120,8 @@ namespace Academy
 		private void buttonAddStudent_Click(object sender, EventArgs e)
 		{
 			studentForm = new StudentForm();
-			studentForm.ShowDialog();
+			if(studentForm.ShowDialog() == DialogResult.OK)
+				tabControl_SelectedIndexChanged (tabControl,null);
 
 		}
 	}
